e1fb1c676a579d9b86964361e574883a03ddd04b
xiwq8 <ch39xie@outlook.com>
2024-06-17 15:37:24 +0800

    Modify least.go

--- least.go
+++ least.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Only truth single administration world college.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
