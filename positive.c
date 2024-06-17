3f05fd97dc69342bebb554bd48d345daa08ce6b8
xiwq8 <ch39xie@outlook.com>
2024-06-17 15:37:21 +0800

    Modify positive.c

--- positive.c
+++ positive.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Feeling shoulder mouth property their performance.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
