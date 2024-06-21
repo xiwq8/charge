6539e4c439ef3dea795cd1c34866b4d0ab55f385
xiwq8 <ch39xie@outlook.com>
2024-06-21 11:25:58 +0800

    Modify against.cpp

--- against.cpp
+++ against.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# War hair true so skill then stuff.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
