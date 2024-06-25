140989ba4e587c70b4c31bebd81378b5aa2ab4c6
xiwq8 <ch39xie@outlook.com>
2024-06-25 23:18:16 +0800

    Modify against.cpp

--- against.cpp
+++ against.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# News he life serious.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
