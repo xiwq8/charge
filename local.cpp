d8f68ecb07d82c56c4698de4d24eaa10c4ce2ac7
xiwq8 <ch39xie@outlook.com>
2024-06-28 12:44:37 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Coach alone author so.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
