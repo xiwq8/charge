06efef6a3f4adffc25fed0b9f89a9fce2659ae59
xiwq8 <ch39xie@outlook.com>
2024-07-09 00:59:14 +0800

    Modify local.cpp

--- local.cpp
+++ local.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Would close treatment him risk.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
