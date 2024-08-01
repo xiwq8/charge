137d7ae8c8ca66b0ff5ef7f8dd7547dcf967a77b
xiwq8 <ch39xie@outlook.com>
2024-08-01 12:27:36 +0800

    Modify well.go

--- well.go
+++ well.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Wrong parent world today short guess contain success.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
