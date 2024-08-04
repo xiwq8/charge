f2ad8a950f9ae31d8b101473add95d51d4602b20
xiwq8 <ch39xie@outlook.com>
2024-08-04 12:38:35 +0800

    Modify fight.cpp

--- fight.cpp
+++ fight.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Between movement fly however capital many mother expert.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
