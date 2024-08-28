3bca23b9aca9bd537d5f4035a912866148b135ef
xiwq8 <ch39xie@outlook.com>
2024-08-28 15:54:05 +0800

    Modify west.c

--- west.c
+++ west.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Hit term affect partner involve factor ground.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
