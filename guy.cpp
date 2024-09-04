c10b240dd247161b12dc9c2b966b789311a29f8b
xiwq8 <ch39xie@outlook.com>
2024-09-04 13:25:06 +0800

    Modify guy.cpp

--- guy.cpp
+++ guy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Share including nature board.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
