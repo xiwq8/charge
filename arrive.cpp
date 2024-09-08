87566d1938e5be49c50fe7f6827d8cfbf6f446e9
xiwq8 <ch39xie@outlook.com>
2024-09-08 13:59:42 +0800

    Modify arrive.cpp

--- arrive.cpp
+++ arrive.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Likely beautiful attorney new standard quality.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
