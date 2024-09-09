7ffcbb2bcf6a90447ae942e58d5b10f84294e697
xiwq8 <ch39xie@outlook.com>
2024-09-09 21:05:08 +0800

    Modify guy.cpp

--- guy.cpp
+++ guy.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Compare friend side bank hair officer government.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
