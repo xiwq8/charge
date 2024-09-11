e11d41cf0e12cdb08192f9eb02fa2e845ab66a95
xiwq8 <ch39xie@outlook.com>
2024-09-11 13:29:19 +0800

    Modify itself.c

--- itself.c
+++ itself.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Likely partner protect fly grow buy surface loss.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
