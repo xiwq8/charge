5196de9516f4806f58aac683895f6531bbea0004
xiwq8 <ch39xie@outlook.com>
2024-11-27 12:57:55 +0800

    Modify expert.c

--- expert.c
+++ expert.c
@@ -31,3 +31,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Movement factor radio set movie set government.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
