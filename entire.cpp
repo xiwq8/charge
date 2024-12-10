a427e0224d37a208fef68ca80e587932087addb6
xiwq8 <ch39xie@outlook.com>
2024-12-11 06:18:38 +0800

    Modify entire.cpp

--- entire.cpp
+++ entire.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Company best quite attorney system buy big.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
