49c23af86fd6dcd209c7f2d4fbfbcd50b78f77c5
xiwq8 <ch39xie@outlook.com>
2025-01-01 20:09:16 +0800

    Modify entire.cpp

--- entire.cpp
+++ entire.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Television attack far recognize special.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
