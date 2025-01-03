d0648942ec3661916258506920333cf6799f3cc9
xiwq8 <ch39xie@outlook.com>
2025-01-03 16:40:12 +0800

    Modify people.c

--- people.c
+++ people.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# New sister road something thousand sound court.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
