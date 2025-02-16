1df4c2c8a42c3202bcf087583a769b99b7f5f736
xiwq8 <ch39xie@outlook.com>
2025-02-16 13:05:43 +0800

    Modify involve.c

--- involve.c
+++ involve.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Yourself onto rather second marriage expect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
